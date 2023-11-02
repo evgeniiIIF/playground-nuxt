0041ceabcd91ab5b3961dd0f017ab40e29bcbbc1
pro100 <pro100.sto@yandex.ru>
2023-11-02 12:59:05 +0300

    add bonus slider

--- src/pages/bonus/index.vue
+++ src/pages/bonus/index.vue
@@ -6,7 +6,9 @@ import { BONUS_CARDS } from './bonus.constants';
 const [isOpenModal, openModal, closeModal] = useBooleanState(false);
 const { isMobile } = useMediaSizes();
 
-const bonusSummaCount = ref(0);
+const bonusSliderPercent = ref(0);
+const howMachSpendCount = computed(() => bonusSliderPercent.value * 1000);
+const bonusAmountCount = computed(() => Math.floor(howMachSpendCount.value * 0.1155));
 </script>
 <template>
   <section class="bonus">
@@ -45,11 +47,34 @@ const bonusSummaCount = ref(0);
             <UIButton text="Узнать подробнее" @click="openModal" />
           </div>
         </div>
-        <div class="bonus__bottom">
-          <h1>bonus__bottom</h1>
-          <div>
-            <Slider v-model="bonusSummaCount" />
+        <div class="bonus__calculate bonus-calculate">
+          <h6 class="bonus-calculate__title">Рассчитайте свою выгоду</h6>
+
+          <div class="bonus-calculate__card card-bonus-calculate">
+            <div class="card-bonus-calculate__top">
+              <div class="card-bonus-calculate__top-title">Сумма которую вы тратите на обслуживание в год</div>
+              <div class="card-bonus-calculate__top-amount">{{ `${howMachSpendCount.toLocaleString('ru-RU')}₽` }}</div>
+            </div>
+            <div class="card-bonus-calculate__slider">
+              <Slider v-model="bonusSliderPercent" :lazy="false" :tooltips="false" orientation="horizontal" />
+            </div>
+            <div class="card-bonus-calculate__bottom">
+              <div class="card-bonus-calculate__bottom-title">Ваша выгода</div>
+              <div class="card-bonus-calculate__bottom-amount">
+                {{ `${bonusAmountCount.toLocaleString('ru-RU')}₽` }}
+              </div>
+              <div class="card-bonus-calculate__bottom-text">
+                Сумма, которую бы Вы сэкономили благодаря бонусной программе
+              </div>
+            </div>
           </div>
+
+          <p class="bonus-calculate__text">
+            *бонусные баллы становятся доступны доступны через 14 дней после покупки и сохраняются втечение 6 месяцев.
+            Бонусная программа не распространяется на приобретение шин и аккумуляторов, услуги тонирования, бронирования
+            автомобиля и на малярно-кузовные работы. Условия бонусной программы могут меняться. Активация карты
+            начинается с первой покупки.
+          </p>
         </div>
       </div>
     </div>
@@ -131,6 +156,8 @@ const bonusSummaCount = ref(0);
       background: #00a19c;
     }
   }
+  &__calculate {
+  }
 }
 
 .card-bonus-small {
@@ -154,6 +181,7 @@ const bonusSummaCount = ref(0);
     color: var(--6, #6b6f6e);
   }
 }
+
 .card-bonus-big {
   max-width: 488px;
   padding: 56px 40px 44px;
@@ -206,6 +234,98 @@ const bonusSummaCount = ref(0);
     @include mobile() {
       font-size: 14px;
     }
+  }
+}
+
+.bonus-calculate {
+  max-width: 590px;
+
+  &__title {
+    margin-bottom: 24px;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 30px; /* 125% */
+    color: var(--4, #3f3f41);
+  }
+  &__card {
+    margin-bottom: 19px;
+  }
+  &__text {
+    font-size: 10px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 15px; /* 150% */
+    color: #b3baba;
+  }
+}
+
+.card-bonus-calculate {
+  padding: 35px 30px;
+  border: 1px solid var(--input-gray, #c4cccc);
+  background: var(--1, #fff);
+  @include mobile() {
+    padding: 25px 20px 14px;
+  }
+
+  &__top {
+    margin-bottom: 17px;
+  }
+  &__top-title {
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 20px; /* 142.857% */
+    color: var(--6, #6b6f6e);
+  }
+  &__top-amount {
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 40px; /* 200% */
+    color: var(--Black, #18243c);
+  }
+  &__slider {
+    margin-bottom: 34px;
+
+    --slider-handle-width: 19px;
+    --slider-handle-height: 19px;
+    --slider-connect-bg: #00a19c;
+    --slider-handle-border: 3px solid #00a19c;
+    --slider-handle-shadow: none;
+
+    .slider-connects {
+      background: #e3e5e5;
+    }
+  }
+  &__bottom {
+  }
+  &__bottom-title {
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 30px; /* 125% */
+    color: var(--4, #3f3f41);
+    @include mobile() {
+      font-size: 20px;
+    }
+  }
+  &__bottom-amount {
+    margin-bottom: 7px;
+
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 40px; /* 133.333% */
+    color: var(--Black, #18243c);
+    unicode-bidi: bidi-override;
+  }
+  &__bottom-text {
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 20px; /* 166.667% */
+    color: #b3baba;
   }
 }
 </style>
