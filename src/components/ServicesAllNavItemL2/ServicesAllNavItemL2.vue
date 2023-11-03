ad1e15da4abfd95b15f4974a84f45bfd1486e829
pro100 <pro100.sto@yandex.ru>
2023-11-03 17:32:17 +0300

    process services-all-nav

---
+++ src/components/ServicesAllNavItemL2/ServicesAllNavItemL2.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import type { ServicesAllItemChild } from '@/store/servicesAll/servicesAll.types';
+
+const { item } = defineProps<{ item: ServicesAllItemChild | undefined }>();
+
+const hasButton = computed(() => item?.children?.length && item?.children?.length > 2);
+const [isOpenList, , , toggleList] = useBooleanState(false);
+
+const children = computed(() => {
+  console.log(hasButton.value, isOpenList.value);
+
+  if (hasButton.value && !isOpenList.value) {
+    return item?.children?.slice(0, 2);
+  }
+
+  return item?.children;
+});
+</script>
+
+<template>
+  <div class="services-all-item-l2">
+    <h3 class="services-all-item-l2__title">
+      {{ item?.title }}
+    </h3>
+    <ul v-if="item?.children" class="services-all-item-l2__list">
+      <li v-for="servicesItemL3 in children" :key="servicesItemL3.id" class="services-all-item-l2__item">
+        <ServicesAllNavItemL3 :item="servicesItemL3" />
+      </li>
+      <div v-if="hasButton" class="services-all-item-l2__button show-button">
+        <button @click="toggleList" class="show-button__button" type="button">
+          <div class="show-button__text">{{ isOpenList ? 'Свернуть' : 'Ещё' }}</div>
+          <div class="show-button__arrow">
+            <IcArrowDown
+              :font-controlled="false"
+              :filled="true"
+              :style="isOpenList ? { transform: 'rotate(180deg)' } : ''"
+            />
+          </div>
+        </button>
+      </div>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+.services-all-item-l2 {
+  &__title {
+    margin-bottom: 9px;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 25px; /* 156.25% */
+    color: #18243c;
+  }
+  &__list {
+    @include mb(15px);
+  }
+  &__item {
+  }
+  &__button {
+  }
+}
+.show-button {
+  &__button {
+    display: flex;
+    align-items: center;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+  }
+  &__text {
+    font-size: 20px;
+    line-height: 30px;
+    color: #00a19c;
+  }
+  &__arrow {
+    display: flex;
+    align-items: center;
+    svg {
+      width: 20px;
+      height: 20px;
+      @include fill-svg-and-path(#00a19c);
+    }
+  }
+}
+</style>
