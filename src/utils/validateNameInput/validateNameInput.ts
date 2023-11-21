ed4ef8fa801debbc1244be018ffe06bbf8aaeb8a
anvyden <anvyden@gmail.com>
2023-11-21 13:16:03 +0700

    add max count of symbols in name input

--- src/utils/validateNameInput/validateNameInput.ts
+++ src/utils/validateNameInput/validateNameInput.ts
@@ -1,6 +1,7 @@
 const validateNameInput = (value: string) => {
   if (value.trim().length < 2) return 'Имя должно состоять из 2 или больше символов';
   if (!value.match(/^[а-я А-Я, a-z A-Z]+$/iu)) return 'Имя не должно содержать цифры и специальные символы';
+  if (value.trim().length > 20) return 'В Имени не должно быть больше 20 символов';
 
   return '';
 };
